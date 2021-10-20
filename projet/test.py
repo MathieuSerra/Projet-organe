7a707a3415be484c926570dd503cdf38e0e4f698
raphaelfilion22 <raphaelfilion22@gmail.com>
2021-10-20 13:16:41 -0400

    Create test.py

---
+++ projet/test.py
@@ -0,0 +1,23 @@
+print ('hello world')
+
+import cv2 as cv
+import numpy as np
+
+#cap=cv.VideoCapture(0)
+
+currentFrame=0
+while(True):
+    ret, frame = cap.read()
+    frame = cv.flip(frame,1)
+    ret,mask = cv.threshold(frame,30,255,cv.THRESH_BINARY)
+    frame=  cv.bitwise_not(frame)
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
+    cv.imshow('frame',gray)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+       break
+
+    currentFrame += 1
+
+cap.release()
+cv.destroyAllWindows()
