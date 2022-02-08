618169101fe6755457e52cc5fdb6d5b8a6f5a4a0
jufila <69648902+jufila@users.noreply.github.com>
2022-02-08 14:40:46 -0500

    update test

--- projet/single_img_tests.py
+++ projet/single_img_tests.py
@@ -5,24 +5,34 @@ import matplotlib as plt
 window_name=' Demo'
 
 img2=cv.imread('fluoro_1.jpg')
-img=cv.imread('test_image.jpg')
-img=img[0:512, 0:512]
+img2=img2[0:512,0:350]
+print(img2.shape)
+img=cv.imread('test_image2.jpg')
+print(img.shape)
+img2=cv.resize(img2,(720,720),interpolation=cv.INTER_AREA)
+img=img[0:720, 300:1020]
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+test=img[:,:,2]
 #img=cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 #TEST1
-twoDimage=img.reshape((-1,3))
+twoDimage=img[:,:,2].reshape((-1,2))
 twoDimage=np.float32(twoDimage)
 print(twoDimage.shape)
-criteria=(cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,2,1.0)
-K=2
-attempts=3
+criteria=(cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,3,1.0)
+K=4
+attempts=5
 ret, label, center=cv.kmeans(twoDimage,K,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
 center=np.uint8(center)
 res=center[label.flatten()]
-result_image=res.reshape((img.shape))
-result_gray=cv.cvtColor(result_image, cv.COLOR_RGB2GRAY)
-ret,th=cv.threshold(result_gray,100,255,cv.THRESH_BINARY_INV)
+result_image=res.reshape((img[:,:,0].shape))
+#result_gray=cv.cvtColor(result_image, cv.COLOR_RGB2GRAY)\
+ret,th=cv.threshold(result_image,120,255,cv.THRESH_BINARY_INV)
+print (result_image.shape)
+th2=cv.adaptiveThreshold(result_image,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,39,7)
+kernel=np.ones((7,7), np.uint8)
+th=cv.morphologyEx(th,cv.MORPH_OPEN, kernel)
+
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret, result=cv.threshold(cv.bitwise_or(gray,th),253,255,cv.THRESH_TOZERO_INV)
 final=cv.addWeighted(result,0.6,cv.cvtColor(img2,cv.COLOR_BGR2GRAY),0.4,0.0)
@@ -62,7 +72,8 @@ final=cv.addWeighted(result,0.6,cv.cvtColor(img2,cv.COLOR_BGR2GRAY),0.4,0.0)
 # edge_closed=cv.morphologyEx(edges,cv.MORPH_CLOSE, kernel)
 # #water=cv.watershed(edges,img)
 
-cv.imshow(window_name,result_gray)
+cv.imshow(window_name,img2)
+#cv.imshow('test',th2)
 
 
 cv.waitKey(0)
--- projet/testvideo.py
+++ projet/testvideo.py
@@ -3,29 +3,35 @@ print ('hello world')
 import cv2 as cv
 import numpy as np
 cap = cv.VideoCapture(1)
-img=cv.imread('fluoro_1.jpg')
+img2=cv.imread('fluoro_1.jpg')
+img2=img2[0:512,0:350]
+img2=cv.resize(img2,(640,480),interpolation=cv.INTER_AREA)
 while True:
     ret,frame=cap.read()
-    width=480
-    height=480
-    sframe=frame[0:height,0:width]
-    simg=img[0:height,0:width]
-    gray_img=cv.cvtColor(simg, cv.COLOR_BGR2GRAY)
-    gray=cv.cvtColor(sframe, cv.COLOR_BGR2GRAY)
-    #f_gray=cv.medianBlur(frame,3)
+    img=frame
+    print(frame.shape)
+    gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    #img=cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    
+    print(img2.shape)
+    #TEST1
+    twoDimage=img[:,:,0].reshape((-1,1))
+    twoDimage=np.float32(twoDimage)
+    print(twoDimage.shape)
+    criteria=(cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,2,1.0)
+    K=3
+    attempts=3
+    ret, label, center=cv.kmeans(twoDimage,K,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
+    center=np.uint8(center)
+    res=center[label.flatten()]
+    result_image=res.reshape((img[:,:,0].shape))
+    #result_image=cv.cvtColor(result_image, cv.COLOR_RGB2GRAY)
+    ret,th=cv.threshold(result_image,100,255,cv.THRESH_BINARY)
+    gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)+50
+    ret, result=cv.threshold(cv.bitwise_or(gray,th),253,255,cv.THRESH_TOZERO_INV)
+    final=cv.addWeighted(result,0.6,cv.cvtColor(img2,cv.COLOR_BGR2GRAY),0.4,0.0)
 
-    _, th1=cv.threshold(gray, np.mean(gray)-20, 255, cv.THRESH_BINARY_INV)
-    #ret, th=cv.threshold(img_gray, 0,255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-    #kernel=np.ones((3,3), np.uint8)
-    #morpho=cv.morphologyEx(th1, cv.MORPH_CLOSE, kernel)
-
-    sum2=cv.bitwise_and(gray,gray,mask=th1)
-    sum2=cv.bitwise_not(sum2)
-    #final=cv.addWeighted(gray_img,0.7,sum2,0.2,0)
-    #final=cv.cvtColor(final,cv.COLOR_BGR2GRAY)
-    #final=cv.bitwise_and(sum2,gray_img)
-
-    cv.imshow('frame',sum2)
+    cv.imshow('frame',result_image)
     if cv.waitKey(1) == ord('q'):
         break
 cap.release()
