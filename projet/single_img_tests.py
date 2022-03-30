9fc1e54f6b1d25670e4778d8ac6d6a7553e4238f
jufila <69648902+jufila@users.noreply.github.com>
2022-03-30 09:07:37 -0400

    fichiers de test image
    ajout du fichier de test d'image et des images traitées et non traitées

--- projet/single_img_tests.py
+++ projet/single_img_tests.py
@@ -1,79 +1,27 @@
 import numpy as np
-import cv2 as cv
+import cv2 as cv2
 import matplotlib as plt
 
 window_name=' Demo'
 
-img2=cv.imread('fluoro_1.jpg')
-img2=img2[0:512,0:350]
-print(img2.shape)
-img=cv.imread('test_image2.jpg')
-print(img.shape)
-img2=cv.resize(img2,(720,720),interpolation=cv.INTER_AREA)
-img=img[0:720, 300:1020]
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-test=img[:,:,2]
-#img=cv.cvtColor(img, cv.COLOR_BGR2RGB)
+img_fluoro=cv2.imread('Test_CNR_SNR.png')
+img_normal=cv2.imread('test_normal.png')
+img_normal=cv2.cvtColor(img_normal,cv2.COLOR_BGR2GRAY)
 
-#TEST1
-twoDimage=img[:,:,2].reshape((-1,2))
-twoDimage=np.float32(twoDimage)
-print(twoDimage.shape)
-criteria=(cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,3,1.0)
-K=4
-attempts=5
-ret, label, center=cv.kmeans(twoDimage,K,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
-center=np.uint8(center)
-res=center[label.flatten()]
-result_image=res.reshape((img[:,:,0].shape))
-#result_gray=cv.cvtColor(result_image, cv.COLOR_RGB2GRAY)\
-ret,th=cv.threshold(result_image,120,255,cv.THRESH_BINARY_INV)
-print (result_image.shape)
-th2=cv.adaptiveThreshold(result_image,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,39,7)
-kernel=np.ones((7,7), np.uint8)
-th=cv.morphologyEx(th,cv.MORPH_OPEN, kernel)
+print(img_fluoro.shape)
+print(img_normal.shape)
 
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-ret, result=cv.threshold(cv.bitwise_or(gray,th),253,255,cv.THRESH_TOZERO_INV)
-final=cv.addWeighted(result,0.6,cv.cvtColor(img2,cv.COLOR_BGR2GRAY),0.4,0.0)
-# print(th.shape)
+img_fluoro=cv2.rectangle(img_fluoro,(235,205),(250,220),(255,0,0),2) #roi 
+img_fluoro=cv2.rectangle(img_fluoro,(280,220),(330,270),(0,0,255),2) #bckg alginate
+img_fluoro=cv2.rectangle(img_fluoro,(380,220),(530,370),(0,255,0),2) #gros bckg
+gray_fluoro=cv2.cvtColor(img_fluoro,cv2.COLOR_BGR2GRAY)
+CNR=abs((np.mean(gray_fluoro[235:250,205:220])-np.mean(gray_fluoro[380:530,220:370])))/np.std(gray_fluoro[380:530,220:370])
+SNR=20*np.log((np.mean(gray_fluoro[235:250,205:220]))/(np.std(gray_fluoro[380:530,220:370])))
+cv2.imshow(window_name,img_fluoro)
 
-#OLD version
+print('Le CNR est de : ', CNR, ' pour l''image choisie')
+print('Le SNR est de : ', SNR, ' pour l''image choisie')
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#img_fluoro=cv2.cvtColor(img_fluoro,cv2.COLOR_BGR2GRAY)
 
-#gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-#_, th1=cv.threshold(gray, np.mean(gray)-20, 255, cv.THRESH_TOZERO)
-
-
-
-
-
-
-#TEST2
-# img=cv.resize(img,(256,256))
-# gray=cv.cvtColor(img,cv.COLOR_RGB2GRAY)
-# _,thresh=cv.threshold(gray,np.mean(gray)-35,255,cv.THRESH_BINARY_INV)
-# edges=cv.dilate(cv.Canny(thresh,0,255),None)
-# #Detecting/drawing contours
-# cnt=sorted(cv.findContours(edges,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)[-2],key=cv.contourArea)[-1]
-# mask=np.zeros((256,256),np.uint8)
-# masked=cv.drawContours(mask,[cnt],-1,255,-1)
-# #segmenting the regions
-# dst=cv.bitwise_and(img,img,mask=mask)
-# segmented=cv.cvtColor(dst,cv.COLOR_BGR2RGB)
-#sucks
-
-#TEST3-thresholding
-
-
-# #Canny edge test
-# edges=cv.Canny(image=img, threshold1=45, threshold2=65)
-# kernel=np.ones((3,3), np.uint8)
-# edge_closed=cv.morphologyEx(edges,cv.MORPH_CLOSE, kernel)
-# #water=cv.watershed(edges,img)
-
-cv.imshow(window_name,img2)
-#cv.imshow('test',th2)
-
-
-cv.waitKey(0)
